9ec46ac15950e28411cf0c9af1ae1c117f3b7957
showermax <sevenhauz@gmail.com>
2023-04-06 23:57:03 +0300

    minus 2 btns

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,11 @@ function App() {
 
   return (
     <div className="App">
-      <div className="wrapper" style={{width: "100vw", height: "100vh", background: "gray", display: "flex"}}>
+      <div className="wrapper" style={{width: "90vw", height: "100vh", background: "gray", display: "flex"}}>
           <div className="sideBar" style={{width: "20%", background: "brown"}}>
               <p>btn</p>
               <p>btn</p>
               <p>btn</p>
-
               <p>btn</p>
               <p>btn</p>
           </div>
