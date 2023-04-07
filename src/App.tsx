8802bdd5dd3fa992e4509bc1f0bcd5bf1a743f7d
sonyhero <27anton06@gmail.com>
2023-04-07 21:19:14 +0300

    test-anton-2

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,7 @@ function App() {
               <p>btn</p>
               <p>btn</p>
               <p>btn</p>
-              <p>btn</p>
-              <p>btn</p>
-              <p>btn</p>
+
           </div>
           <div className="contentWrapper" style={{width: "80%", background: "gray"}}>
               <div className="header" style={{display:"flex", justifyContent: "space-between", background: "green"}}>
